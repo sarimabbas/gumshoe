e16d92b32ecc8f3003333f65b2ad9c75b4db3728
Sarim Abbas <3000809+sarimabbas@users.noreply.github.com>
2024-06-24 19:53:47 -0700

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import "./App.css";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "./components/ui/resizable";
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <ResizablePanelGroup direction="horizontal">
+      {/* sidebar */}
+      <ResizablePanel
+        className="h-screen p-4"
+        minSize={20}
+        defaultSize={20}
+        maxSize={30}
+      >
+        One
+      </ResizablePanel>
+      <ResizableHandle withHandle />
+      {/* browser */}
+      <ResizablePanel className="p-4">
+        <h1></h1>
+      </ResizablePanel>
+    </ResizablePanelGroup>
+  );
 }
 
 export default App;
